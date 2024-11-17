99484a58339fa756ff12b32f97cba87b6379a7eb
Rohan <rohan151025@gmail.com>
2024-11-17 13:27:09 +0530

    Console version is working almost
    console version of tic tac toe is working with a new error - loop runs
    infinitely that is cannot tie, one has to win to end the game;
    also removed the differ attribute from script tag in html, it was
    causing an issue of not printing the gameBoard at time.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// prints different type of error messages based on the errorCode and hint
 function printError(errorCode, hint) {
     switch (errorCode) {
         case 1:
@@ -7,125 +8,146 @@ function printError(errorCode, hint) {
             console.log(`${hint} is not empty, choose different index`);
             break;
         default:
-            console.log('An unknown error happened');
-
+            console.log(`An unknown error happened at ${errorCode} js line number`);
     }
 }
-
 
 function GameBoard() {
 
-    let gameBoard = Array.from({ length: 9 }, (_, index) => index+1);
+let gameBoard = Array.from({ length: 9 }, (_, index) => index+1);
 
-    const printBoard = () => {
-        for (let x = 0; x <= 6; x += 3) {
-            console.log(`${gameBoard[x].symbol} | ${gameBoard[x+1].symbol} | ${gameBoard[x+2].symbol}`);
+// print the current gameBoard array
+const printBoard = () => {
+    for (let x = 0; x <= 6; x += 3) {
+        console.log(`${gameBoard[x]} | ${gameBoard[x+1]} | ${gameBoard[x+2]}`);
+    }
+    console.log('');
+}
+
+// checks for any winner, then returns the winner (if any) otherwise returns null
+const checkWinner = () => {
+    for (let x = 0; x <= 6; x = x+3) {
+        if (gameBoard[x] === gameBoard[x+1] && gameBoard[x+1] === gameBoard[x+2]) {
+            return gameBoard[x];
         }
     }
 
-    const checkWinner = () => {
-        for (let x = 0; x <= 6; x = x+3) {
-            if (gameBoard[x] === gameBoard[x+1] === gameBoard[x+2]) {
-                return gameBoard[x];
-            }
+    for (let x = 0; x <= 2; x++) {
+        if (gameBoard[x] === gameBoard[x+3] && gameBoard[x+3] === gameBoard[x+6]) {
+            return gameBoard[x];
         }
-    
-        for (let x = 0; x <= 2; x++) {
-            if (gameBoard[x] === gameBoard[x+3] === gameBoard[x+6]) {
-                return gameBoard[x];
-            }
-        }
-    
-        if (gameBoard[0] === gameBoard[4] === gameBoard[8]) return gameBoard[0];
-        if (gameBoard[2] === gameBoard[4] === gameBoard[6]) return gameBoard[2];
     }
 
-    return {printBoard, checkWinner};
+    if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) return gameBoard[0];
+    else if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) return gameBoard[2];
+    else return null;
+}
+
+// only function which can edit the gameBoard array
+// takes the symbol(2nd arg) and put it at the index(1st arg) specified
+const playTurn = (indexx, symbol) => {
+    gameBoard[indexx] = symbol;
+}
+
+// returns the element at the indexx(1st arg) from the gameBoard array
+const checkAtIndex = (indexx) => {
+    return gameBoard[indexx];
+}
+
+return {printBoard, checkWinner, playTurn, checkAtIndex};
+}
+
+// return an array of 2 player objects, each object contains a name and a symbol
+const newPlayers = () => {
+
+    let name1 = prompt("Player 1's name: ");
+    console.log(`${name1} your symbol is: X`);
+
+    let name2 = prompt("Player 2's name: ");
+    console.log(`${name2} your symbol is: O`);
+
+    const player1 = { name: name1, symbol: 'X', };
+    const player2 = { name: name2, symbol: 'O', };
+
+    return [player1, player2];
 }
 
 
-(function GameController() {
+const [player1, player2] = newPlayers(); 
+let activePlayer = player1;  // activePlayer refers an object
 
-    const board = GameBoard();
 
-    const newPlayers = () => {
+// asks for an input from the activePlayer until input is integer from 1 to 9 and then return that "input - 1"
+const playerInput = () => {
+    let inputt;
+    while (true) {
+        inputt = Number(prompt(`${activePlayer.name} with symbol ${activePlayer.symbol} turn, enter index: `));
 
-        let name1 = prompt("Player 1's name: ");
-        console.log(`${name1} your symbol is: X`);
-        
-        let name2 = prompt("Player 2's name: ");
-        console.log(`${name2} your symbol is: O`);
-    
-        const player1 = { name: name1, symbol: 'X', };
-        const player2 = { name: name2, symbol: 'O', };
-    
-        return [player1, player2];
-    }
-
-    const playerInput = () => {
-        let inputt;
-        while (true) {
-            inputt = Number(prompt(`${activePlayer.name} turn, enter index: `));
-    
-            if (isNaN(inputt) || inputt > 9 || inputt < 1) {
-                printError(1, inputt);
-                continue;
-            }
-            break;
+        if (isNaN(inputt) || inputt > 9 || inputt < 1) {
+            printError(1, inputt);
+            continue;
         }
+        break;
+    }
+
+    return inputt-1;
+}
+
+
+// when called switches the current active player
+const switchActivePlayer = () => {
+    (activePlayer === player1) ? (activePlayer = player2) : (activePlayer = player1);
+};
+
+let board = GameBoard();
+board.printBoard();
+
+
+// plays a single turn, then prints current board, then switches active player
+const semiRound = () => {
+    let indexx;
     
-        return inputt-1;
-    }
-
-    const switchActivePlayer = () => {
-        (activePlayer == player1) ? (activePlayer = player2) : (activePlayer = player1);
-    }
-
-    const semiRound = () => {
-        let indexx;
-        
-        while(true) {
-            indexx = playerInput(activePlayer);
-        
-            if (!(typeof board[indexx] === 'number')) {
-                printError(2, indexx);
-                continue;
-            }
-            board[indexx] = activePlayer;
-            break;
+    while(true) {
+        indexx = playerInput();
+    
+    if (typeof board.checkAtIndex(indexx) === 'object') {
+            printError(2, indexx);
+            continue;
         }
-        switchActivePlayer();
+        board.playTurn(indexx, activePlayer.symbol);
+        break;
     }
+    board.printBoard();
+    switchActivePlayer();
+}
 
 
-    const announceWinner = (player) => {
-        console.log(`${player.name} with symbol ${player.symbol} wins !`);
-    } 
+// takes a player object and logs it as the winner
+const announceWinner = (winner) => {
+    (winner === 'X') ? (winner = player1) : ((winner === 'O') ? (winner = player2) : (printError(143)));
+    console.log(`${winner.name} with symbol ${winner.symbol} wins !`);
+} 
 
 
-
-    board.printBoard();  // print the empty board
-    const [player1, player2] = newPlayers();  // ask for the player's name
-    let activePlayer = player1; // player1 will start the game by default
+// runs the 9 complete rounds then declared tie!!
+let rounds = 0;
+for (; rounds < 9; rounds++) {
 
     let winner;
 
-    for (let rounds = 0; rounds < 9; rounds++) {
-
-        semiRound();
-        winner = board.checkWinner();
-        if (!winner == false) {
-            announceWinner(winner);
-            break;
-        }
-
-        semiRound();
-        winner = board.checkWinner();
-        if (!winner == false) {
-            announceWinner(winner);
-            break;
-        }
+    semiRound();
+    winner = board.checkWinner();
+    if (winner !== null) {
+        announceWinner(winner);
+        break;
     }
 
-    console.log('TIE !!')
-})();
+    semiRound();
+    winner = board.checkWinner();
+    if (winner !== null) {
+        announceWinner(winner);
+        break;
+    }
+}
+
+if (rounds === 9) console.log('TIE !!')
